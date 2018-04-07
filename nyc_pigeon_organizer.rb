6642b196b8056c43970965204ad24f730909f8ba
Richard Kim <richeekim@gmail.com>
2018-04-07 23:02:23 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:lives].each do |location, pigeons|
+    pigeons.each do |pigeon|
+      pigeon_list[pigeon] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+    end
+  end
+      pigeon_list.each do |pigeon, info|
+        data.each do |attribute, values|
+         values.each do |value, pigeon_names|
+           pigeon_names.each do |name|
+            if name == pigeon
+              pigeon_list[pigeon][attribute] << value.to_s
+            end
+          end
+        end
+      end
+    end
+  return pigeon_list
 end
